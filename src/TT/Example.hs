3303ecaadb660d876d41e5c8a56fb5a1a561db7c
Jon Sterling <jon@jonmsterling.com>
2015-01-04 22:31:55 -0800

    example of function equivalence proof

--- src/TT/Example.hs
+++ src/TT/Example.hs
@@ -1,14 +1,18 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module TT.Example where
 
 import TT.Operator
 import TT.Model
+import TT.Judge
 import TT.Monad
 
 import Abt.Concrete.LocallyNameless
 import Abt.Class
+import Control.Monad hiding (void)
 import Control.Monad.Gen
+import Data.Monoid
 import Prelude hiding (pi)
 
 test ∷ M e (Tm0 Op)
@@ -17,8 +21,24 @@ test = do
   nbe (pi void (x \\ void)) $ 
     lam (x \\ var x)
 
-
 main ∷ IO ()
 main = do
   str ← either fail return . runGenT . _M $ test >>= toString
   print str
+
+  res ← either (const $ fail "error") return . runGenT . _M $ do
+    checkType mempty unit (ax ∷ Tm0 Op)
+
+    x ← fresh
+    let
+      f = lam (x \\ var x)
+      g = lam (x \\ ax)
+      ty = pi unit (x \\ unit)
+
+    y ← fresh
+    p ← fresh
+
+    checkType mempty (eq ty ty f g) $ lam (x \\ lam (y \\ (lam (p \\ ax))))
+
+        
+  print res
